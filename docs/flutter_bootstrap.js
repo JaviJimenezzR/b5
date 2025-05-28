1bc2b20c1a039b8561da8bede00c87faf29691ed
Javier Jimenez <fjjr2000@gmail.com>
2025-05-28 17:13:52 +0200

    Publicar app correctamente en GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,10 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-
-    serviceWorkerVersion: "1868491499"
-=======
-    serviceWorkerVersion: "779800731"
->>>>>>> 1922eb3 (Primer commit para publicar en GitHub Pages)
+    serviceWorkerVersion: "576633532"
   }
 });
